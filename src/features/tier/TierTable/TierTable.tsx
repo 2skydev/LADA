63ac67309a1b68d9dad82c541aff87e8e1bca0b2
2sky <2skydev@gmail.com>
2023-03-06 00:34:58 +0900

    리팩토링 중 경로 버그 수정

--- src/features/tier/TierTable/TierTable.tsx
+++ src/features/tier/TierTable/TierTable.tsx
@@ -5,9 +5,9 @@ import { Table, Tooltip } from 'antd';
 import clsx from 'clsx';
 import dayjs from 'dayjs';
 
-import LaneSelect from '~/components/LaneSelect';
 import RankingVariation from '~/components/RankingVariation';
 import ChampionProfileSmall from '~/features/asset/ChampionProfileSmall';
+import LaneSelect from '~/features/lane/LaneSelect';
 import useAPI from '~/hooks/useAPI';
 import { useCustomForm } from '~/hooks/useCustomForm';
 
