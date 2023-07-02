f7740f690f662ecb3bcd18eecf1de0b4af0b63ce
2sky <2skydev@gmail.com>
2023-07-02 23:50:54 +0900

    🐛 fix: 롤 클라이언트 끄고 킬 때 현재 소환사 정보 안가져오는 버그 수정

--- src/renderer/src/hooks/listener/useAppStateListener.ts
+++ src/renderer/src/hooks/listener/useAppStateListener.ts
@@ -1,11 +1,13 @@
 import { useEffect } from 'react'
 
-import { useRecoilState } from 'recoil'
+import { useRecoilState, useSetRecoilState } from 'recoil'
 
 import { appStateStore } from '@renderer/stores/app'
+import { currentSummonerStore, getCurrentSummoner } from '@renderer/stores/currentSummoner'
 
 const useAppStateListener = () => {
   const [appState, setAppState] = useRecoilState(appStateStore)
+  const setCurrentSummoner = useSetRecoilState(currentSummonerStore)
 
   const register = async () => {
     const isReady = await window.electron.apis('league', '/is-ready')
@@ -15,11 +17,16 @@ const useAppStateListener = () => {
       leagueIsReady: isReady,
     })
 
-    window.electron.subscribeLeague('connect-change', state => {
+    window.electron.subscribeLeague('connect-change', async state => {
+      const leagueIsReady = state === 'connect'
+
       setAppState({
         ...appState,
-        leagueIsReady: state === 'connect',
+        leagueIsReady,
       })
+
+      const currentSummoner = await getCurrentSummoner({ checkIsReady: true })
+      setCurrentSummoner(currentSummoner)
     })
 
     window.electron.subscribeLeague('in-game', isInGame => {
--- src/renderer/src/stores/currentSummoner.ts
+++ src/renderer/src/stores/currentSummoner.ts
@@ -7,24 +7,38 @@ export interface CurrentSummonerStoreValues {
   psId: string
 }
 
+export interface GetCurrentSummonerOptions {
+  checkIsReady?: boolean
+  preparedData?: any
+}
+
+export const getCurrentSummoner = async (
+  options: GetCurrentSummonerOptions = {},
+): Promise<CurrentSummonerStoreValues | null> => {
+  let data = options.preparedData
+
+  if (options.checkIsReady) {
+    const isReady = await window.electron.apis('league', '/is-ready')
+    if (!isReady) return null
+  }
+
+  if (!data) {
+    data = await window.electron.apis('league', '/summoner/current')
+  }
+
+  if (!data) return null
+
+  const psId = await window.electron.apis('ps', `/summoner-ps-id/${data.displayName}`)
+
+  return {
+    id: data.summonerId,
+    name: data.displayName,
+    profileIconId: data.profileIconId,
+    psId,
+  }
+}
+
 export const currentSummonerStore = atom<CurrentSummonerStoreValues | null>({
   key: 'current-summoner',
-  default: (async () => {
-    const isReady = await window.electron.apis('league', '/is-ready')
-
-    if (!isReady) return null
-
-    const data = await window.electron.apis('league', '/summoner/current')
-
-    if (!data) return null
-
-    const psId = await window.electron.apis('ps', `/summoner-ps-id/${data.displayName}`)
-
-    return {
-      id: data.summonerId,
-      name: data.displayName,
-      profileIconId: data.profileIconId,
-      psId,
-    }
-  })(),
+  default: getCurrentSummoner({ checkIsReady: true }),
 })
