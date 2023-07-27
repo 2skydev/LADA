7cc88a4e2f2ca396c5a55e64410273466de8d4ad
2sky <2skydev@gmail.com>
2023-07-27 19:25:16 +0900

    ✨ feat: 챔피언 선택 정보 없을 때 예외 처리

--- src/renderer/src/pages/live/champion-select.tsx
+++ src/renderer/src/pages/live/champion-select.tsx
@@ -1,13 +1,23 @@
+import { useAtomValue } from 'jotai'
+
 import LayoutConfig from '@renderer/components/LayoutConfig'
 import ChampDetail from '@renderer/features/champ/ChampDetail/ChampDetail'
+import LiveChampionSelectNotFound from '@renderer/features/empty/LiveChampionSelectNotFound'
+import { championSelectSessionAtom } from '@renderer/stores/atoms/championSelectSession.atom'
 import { LiveChampionSelectPageStyled } from '@renderer/styles/pageStyled/liveChampionSelectPageStyled'
 
 const LiveChampionSelect = () => {
+  const { championId, tempChampionId } = useAtomValue(championSelectSessionAtom) || {}
+
+  const id = championId || tempChampionId
+
   return (
     <LiveChampionSelectPageStyled>
       <LayoutConfig breadcrumbs={['라이브 게임', '챔피언 선택']} />
 
-      {/* <ChampDetail /> */}
+      {!id && <LiveChampionSelectNotFound />}
+
+      {id && <ChampDetail champId={Number(id)} />}
     </LiveChampionSelectPageStyled>
   )
 }
